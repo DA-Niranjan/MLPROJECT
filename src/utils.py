91ab3d6429e5ab5a98a81256128b3d238f233923
DA-Niranjan <niranjansable18@gmail.com>
2024-06-17 15:59:59 +0530

    Creating the Application

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ import sys
 
 import numpy as np
 import pandas as pd
-import dill
+import pickle
 
 from sklearn.metrics import r2_score
 
@@ -16,7 +16,7 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
         
         with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)
+            pickle.dump(obj, file_obj)
             
     except Exception as e:
         raise CustomException(e, sys)
@@ -46,5 +46,13 @@ def evaluate_models(X_train,X_test,y_train,y_test,models):
             
     except Exception as e:
         raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+          
+    except Exception as e:
+        raise CustomException(e,sys)
         
     
